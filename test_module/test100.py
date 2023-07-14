20b9f8103b2d4ee979f39387f3908d0d6bffee23
gaozhiwei <854433859@qq.com>
2023-07-14 15:21:00 +0800

    fix

--- test_module/test100.py
+++ test_module/test100.py
@@ -1,13 +1,14 @@
-num_list = []
-for i in range(1, 100):
-    if i % 7 == 0:
-        num_list.append(i)
-    if len(str(i)) <= 1:
-        continue
-    if list(str(i))[-1] == "7" and i not in num_list:
-        num_list.append(i)
+def seven_end(begin_num, end_num):
+    num_list = []
+    for i in range(begin_num, end_num):
+        if i % 7 == 0:
+            num_list.append(i)
+        if len(str(i)) <= 1:
+            continue
+        if list(str(i))[-1] == "7" and i not in num_list:
+            num_list.append(i)
+    return num_list
 
-print(num_list)
 
-print(len(num_list))
-
+seven_list = seven_end(1, 100)
+print(seven_list)
