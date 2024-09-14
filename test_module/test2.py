188a3679994cc5b61bd694cc11f0f3451e17586b
gaozhiwei <854433859@qq.com>
2024-09-14 14:49:11 +0800

    add

--- test_module/test2.py
+++ test_module/test2.py
@@ -1,5 +1,16 @@
-str_test = ""
+def is_palindrome(string):
+    return string == string[::-1]
 
 
-print(len(str_test))
+def count_palindrome(string):
+    count = 0
+    for i in range(len(string)):
+        for j in range(i+1, len(string) + 1):
+            if is_palindrome(string[i:j]):
+                count += 1
+    return count
 
+
+string = "abcba"
+count = count_palindrome(string)
+print(count)
