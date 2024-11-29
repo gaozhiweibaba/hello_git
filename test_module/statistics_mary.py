1ae236988ef310bf585e3524f94e2dd86dc4143c
gaozhiwei <854433859@qq.com>
2024-11-29 14:56:20 +0800

    修改接口入参方式

--- test_module/statistics_mary.py
+++ test_module/statistics_mary.py
@@ -14,7 +14,7 @@ def get_market_value(task_id: str):
     headers = {
         'accept': 'application/json, text/plain, */*',
         'accept-language': 'zh-CN,zh;q=0.9',
-        'authorization': 'Basic 34JeXSoVfvgoSSqPsmSZtjSQuhAz0v',
+        'authorization': 'Basic pWSRKJ8dGDhSQrziV4I2aOn72CraRj',
         'origin': 'https://crm-dev.tigerbrokers.net',
         'priority': 'u=1, i',
         'referer': 'https://crm-dev.tigerbrokers.net/',
@@ -31,7 +31,7 @@ def get_market_value(task_id: str):
     return json.loads(data.text)["data"]['marketValue']
 
 
-def get_allocated_value(page_size: int, allocated_at: list, belong_crm_name: str):
+def get_allocated_value(**kwargs):
     current_timestamp = int(time.time() * 1000)
     url = 'https://noldor-test.qa.tigerbrokers.net/api/polaris/v1/transfer_task/list'
     params = {
@@ -42,7 +42,7 @@ def get_allocated_value(page_size: int, allocated_at: list, belong_crm_name: str
     headers = {
         'accept': 'application/json, text/plain, */*',
         'accept-language': 'zh-CN,zh;q=0.9',
-        'authorization': 'Basic 34JeXSoVfvgoSSqPsmSZtjSQuhAz0v',
+        'authorization': 'Basic pWSRKJ8dGDhSQrziV4I2aOn72CraRj',
         'content-type': 'application/json;charset=UTF-8',
         'origin': 'https://crm-dev.tigerbrokers.net',
         'priority': 'u=1, i',
@@ -55,26 +55,24 @@ def get_allocated_value(page_size: int, allocated_at: list, belong_crm_name: str
         'sec-fetch-site': 'same-site',
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
     }
-    data = {
-        'pageNumber': 1,
-        'pageSize': page_size,
-        'belongCrm': "83499517",
-        'allocatedAt': allocated_at,
-        'belongCrmName': belong_crm_name
-    }
-
-    reqs = requests.post(url=url, params=params, headers=headers, data=json.dumps(data))
-    return json.loads(reqs.text)["data"]['list']
+    print(kwargs)
+    reqs = requests.post(url=url, params=params, headers=headers, data=json.dumps(kwargs))
+    try:
+        return json.loads(reqs.text)["data"]['list']
+    except Exception as e:
+        print("token 过期", e)
 
 
+# 转股数据汇总表
 def count_allocated_value():
-    content = get_allocated_value(page_size=200, allocated_at=[1730390400000, 1732982399999], belong_crm_name='高志伟(gaozhiwei)')
+    content = get_allocated_value(pageNumber=1, pageSize=2000, allocatedAt=[1730390400000, 1732982399999], belongCrm='83499517', belongCrmName="高志伟(gaozhiwei)")
     count = 0
     us_mary = 0
     hk_mary = 0
     sg_mary = 0
     au_mary = 0
     for i in content:
+        print(i)
         count += 1
         if i["market"] == "US":
             us_count = get_market_value(str(i["id"]))
@@ -95,6 +93,9 @@ def count_allocated_value():
     print("分配任务市值（SG）：", sg_mary)
     print("分配任务市值（AU）：", au_mary)
     print("总计-市值：", total_mary)
+
+
+# def reject_summery_list():
 
 
 if __name__ == '__main__':
